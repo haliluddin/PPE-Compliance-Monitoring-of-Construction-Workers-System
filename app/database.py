f6f6cb10ec73f88b84555c6a9690ec774b1fa7a1
Naila Haliluddin <hnailataji@gmail.com>
2025-10-11 10:08:16 +0800

    testing runpod gpu

--- app/database.py
+++ app/database.py
@@ -6,7 +6,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 #DATABASE_URL = os.environ.get("POSTGRES_URL", "postgresql://postgres:ppe@localhost:5432/ppe_compliance")
 DATABASE_URL = os.environ.get("POSTGRES_URL", "postgresql://myuser:password@postgres:5432/ppe_compliance")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, pool_pre_ping=True)
+else:
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
